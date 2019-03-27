c5fc998dba5266efb43f6ef0ba0a26d656755eab
khamani <banti.sharma@gmail.com>
2019-03-27 18:30:45 +0530

    added the html reporter

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   globalSetup: './setup.js',
   globalTeardown: './teardown.js',
   testEnvironment: './puppeteer_environment.js',
+  reporters: [
+    "jest-spec-reporter",
+    "jest-html-reporters"
+  ],
 }
