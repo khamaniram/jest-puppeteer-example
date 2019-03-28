407a5f52f39719456fc5946f27b18b1fccdb1d1a
khamani <banti.sharma@gmail.com>
2019-03-28 22:56:41 +0530

    Added some flags for smooth execution of headless chrome

--- __tests__/test5.js
+++ __tests__/test5.js
@@ -1,12 +1,11 @@
-const timeout = 50000;
-jest.setTimeout(timeout);
+jest.setTimeout(10000);
 describe(  'Home Page',  () => {
     let page
     beforeAll(async () => {
       page = await global.__BROWSER__.newPage();
       page.setViewport({width:1024, height:1024});
       await page.goto('https://google.com',{ timeout: 0});
-    }, timeout);
+    });
 
     afterAll(async () => {
       await page.close();
@@ -16,4 +15,4 @@ describe(  'Home Page',  () => {
       let text = await page.evaluate(() => document.body.textContent);
       expect(text).toContain('google');
     });
-  },  timeout);
+  } );
